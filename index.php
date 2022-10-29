ad3241f77cfda202e1d815fb579ceb40ba589148
= <=>
2022-10-29 10:06:08 +0700

    sdf

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
 
 <div id="fullpage">
     <div class="section trangchu" id="section1">
-        <video id="myVideo" loop muted autoplay playsinline controls>
+        <video id="myVideo"  autoplay playsinline >
             <source src="imgs/flowers.mp4" type="video/mp4">
             <source src="imgs/flowers.webm" type="video/webm">
             <source src="imgs/flowers.ogg" type="video/ogg">
